ae915124bd961aab299699aa50699c3fd1a76cda
tamakiii <tamakiii@users.noreply.github.com>
2020-02-08 10:13:25 +0900

    multi stage build

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,10 @@
-FROM rust:1.41.0-alpine3.11
+FROM rust:1.41.0-alpine3.11 AS production-pseudo
 
 RUN apk add --no-cache make
+
+# --
+
+FROM production-pseudo AS development
+
+ENV PAGER=less
+RUN apk add --no-cache man man-pages coreutils-doc cargo-doc
--- docker.mk
+++ docker.mk
@@ -1,14 +1,27 @@
-.PHONY: run build clean
+.PHONY: run build ash clean
 
 NAME := tamakiii-sandbox/hello-rust
 WORK := /app
+ENVIRONMENT := $(shell grep '^ENVIRONMENT=' .env | sed -E 's/ENVIRONMENT=//')
+
+init: \
+	.env \
+	build
+
+.env:
+	touch $@
+	echo "ENVIRONMENT=production-pseudo" >> $@
 
 run:
 	docker run --rm -it -v $(PWD):$(WORK) -w $(WORK) $(NAME) make run
 
+ash:
+	docker run --rm -it -v $(PWD):$(WORK) -w $(WORK) $(NAME) $@
+
 build:
-	docker build -t $(NAME) .
+	docker build -t $(NAME) --target $(ENVIRONMENT) .
 
 clean:
+	rm .env
 	docker image rm $(NAME)
 
