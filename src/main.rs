df78d7f3011fe4800f7637a22da0c96199b1d745
tamakiii <tamakiii@users.noreply.github.com>
2020-02-08 12:13:28 +0900

    using rand::Rng;

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
@@ -9,5 +10,8 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("failed to read line!!");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
     println!("You guessed: {}", guess);
+    println!("The secret number is: {}", secret_number);
 }
