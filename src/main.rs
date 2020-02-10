cf207e57deb0d5dfbe1c166ce1eb36dc3f22bddb
tamakiii <tamakiii@users.noreply.github.com>
2020-02-10 18:09:46 +0900

    impl with hyper

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
-use std::io;
-use rand::Rng;
+// extern crate hyper;
+// extern crate reroute;
 
-fn main() {
-    println!("Guess the number");
-    println!("Please input your gess.");
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
-    let mut guess = String::new();
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+// use reroute::{Captures, Router};
 
-    io::stdin().read_line(&mut guess)
-        .expect("failed to read line!!");
+async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Hello, World")))
+}
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    pretty_env_logger::init();
 
-    println!("You guessed: {}", guess);
-    println!("The secret number is: {}", secret_number);
+    let make_svc = make_service_fn(|_conn| {
+        async { Ok::<_, Infallible>(service_fn(hello)) }
+    });
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000)).into();
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+
+    return Ok(());
 }
