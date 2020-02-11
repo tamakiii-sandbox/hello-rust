65bd0b09892f8c08e44ef89e93c360157b5675d3
tamakiii <tamakiii@users.noreply.github.com>
2020-02-11 16:40:04 +0900

    complete rnd

--- src/rand.rs
+++ src/rand.rs
@@ -1,24 +1,38 @@
 extern crate rand;
 
 use std::io;
+// use std::io::Write;
 use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-
     let secret_number = ::rand::thread_rng().gen_range(1, 101);
-    let guessed_number : i32 = guess.trim().parse()
-        .expect("Please type a number!");
 
-    match guessed_number.cmp(&secret_number) {
-        Ordering::Less => println!("Too small! {} < {}", guessed_number, secret_number),
-        Ordering::Equal => println!("You win! {} = {}", guessed_number, secret_number),
-        Ordering::Greater => println!("Too big! {} > {}", guessed_number, secret_number),
+    loop {
+        let mut guess = String::new();
+
+        println!("Guess the number: ");
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("you guessed: {}", guess);
+
+        let guessed_number : u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("huh? {}", guess);
+                continue;
+            },
+        };
+
+        match guessed_number.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
