3fe023f977c768605dc2fa97162491fad5f62d64
tamakiii <tamakiii@users.noreply.github.com>
2020-02-12 15:26:15 +0900

    cargo fmt

--- src/rand.rs
+++ src/rand.rs
@@ -2,8 +2,8 @@ extern crate rand;
 
 use std::io;
 // use std::io::Write;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     let secret_number = ::rand::thread_rng().gen_range(1, 101);
@@ -13,17 +13,18 @@ fn main() {
 
         println!("Guess the number: ");
 
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
         println!("you guessed: {}", guess);
 
-        let guessed_number : u32 = match guess.trim().parse() {
+        let guessed_number: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("huh? {}", guess);
                 continue;
-            },
+            }
         };
 
         match guessed_number.cmp(&secret_number) {
