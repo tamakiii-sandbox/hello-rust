4411931d2370415cb781f867786900f0d2370300
tamakiii <tamakiii@users.noreply.github.com>
2020-02-15 15:23:17 +0900

    add volumes

--- docker.mk
+++ docker.mk
@@ -1,9 +1,12 @@
 .PHONY: init build ash bash run run/% make/% clean
 .DEFAULT_GOAL := help
 
+DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 NAME := tamakiii-sandbox/hello-rust
 WORK := /app
 ENVIRONMENT := $(shell grep '^ENVIRONMENT=' .env | sed -E 's/ENVIRONMENT=//')
+VOLUMES := $(DIR):$(WORK) ~/.gitignore:/root/.gitignore ~/.config/git/ignore:/root/.config/git/ignore
+
 CMD := bash
 VSC_NAME := vsc-hello-rust-
 
@@ -22,16 +25,16 @@ build:
 	docker build -t $(NAME) --target $(ENVIRONMENT) .
 
 run:
-	docker run --rm -it -v $(PWD):$(WORK) -w $(WORK) $(NAME) make run
+	docker run --rm -it $(foreach v,$(VOLUMES),-v $v) -w $(WORK) $(NAME) make run
 
 ash: run/ash
 bash: run/bash
 
 run/%:
-	docker run --rm -it -v $(PWD):$(WORK) -w $(WORK) $(NAME) $(@F)
+	docker run --rm -it $(foreach v,$(VOLUMES),-v $v) -w $(WORK) $(NAME) $(@F)
 
 make/%:
-	docker run --rm -it -v $(PWD):$(WORK) -w $(WORK) $(NAME) make $(@F)
+	docker run --rm -it $(foreach v,$(VOLUMES),-v $v) -w $(WORK) $(NAME) make $(@F)
 
 vsc:
 	docker exec -it -w /workspaces/$(shell basename $(realpath $(dir $(lastword $(MAKEFILE_LIST))))) $$(docker container ls | grep $(VSC_NAME) | awk '{ print $$1 }') $(CMD)
