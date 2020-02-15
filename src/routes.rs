2d4673b378d2bda8c04e1a3f75ee1c192b6a0128
tamakiii <tamakiii@users.noreply.github.com>
2020-02-15 13:04:49 +0900

    responding Json

--- src/routes.rs
+++ src/routes.rs
@@ -1,5 +1,6 @@
 // #![feature(decl_macro)]
 
+use rocket::response::status;
 use rocket_contrib::json::Json;
 
 use crate::models::Todo;
@@ -10,28 +11,26 @@ pub fn index() -> &'static str {
 }
 
 #[get("/todos")]
-pub fn todos() -> Json<Vec<Todo>> {
-    Json(vec![Todo {
-        id: 1,
-        title: "Read rocket tutorial".into(),
-        description: "Read https://rocket.rs/guide/quickstart/".into(),
-        done: false,
-    }])
+pub fn todos() -> status::Accepted<Json<Vec<Todo>>> {
+    status::Accepted(Some(Json(vec![sample_entity(1)])))
 }
 
 #[post("/todo", data = "<todo>")]
-pub fn todo_post(todo: Json<Todo>) -> String {
-    format!("Accepted post request: {:?}", todo.0)
+pub fn todo_post(todo: Json<Todo>) -> status::Created<Json<Todo>> {
+    status::Created(format!("Accepted post request: {:?}", todo.0), Some(todo))
 }
 
 #[get("/todo/<id>")]
-pub fn todo(id: u32) -> String {
-    let todo = Todo {
-        id,
+pub fn todo(id: u32) -> Json<Todo> {
+    let todo = sample_entity(id);
+    Json(todo)
+}
+
+fn sample_entity(id: u32) -> Todo {
+    Todo {
+        id: id,
         title: "Read rocket tutorial".into(),
         description: "Read https://rocket.rs/guide/quickstart/".into(),
         done: false,
-    };
-
-    format!("{:?}", todo)
+    }
 }
