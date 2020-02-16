d67fc4de65f92f028048eba7ccd056b81a7d0c2a
tamakiii <tamakiii@users.noreply.github.com>
2020-02-16 12:24:26 +0900

    impl routes using cookie

--- src/rocket-requests.rs
+++ src/rocket-requests.rs
@@ -34,8 +34,36 @@ fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new(DIR_ROOT).join(file)).ok()
 }
 
+mod cookies {
+    use rocket::http::{Cookie, Cookies};
+
+    #[get("/")]
+    pub fn index(cookies: Cookies) -> String {
+        let results = cookies.get("count")
+            .map(|value| format!("Message: {}", value));
+
+        match results {
+            Some(r) => r,
+            None => format!("Nothing found"),
+        }
+    }
+
+    #[get("/++")]
+    pub fn increment(mut cookies: Cookies) -> String {
+        let count: i32 = match cookies.get("count") {
+            Some(c) => c.value().parse::<i32>().unwrap() + 1,
+            None => 0,
+        };
+
+        cookies.add(Cookie::new("count", count.to_string()));
+
+        format!("count: {}", count)
+    }
+}
+
 fn main() {
     rocket::ignite()
         .mount("/", routes![index, hello, pages, files])
+        .mount("/cookies", routes![cookies::index, cookies::increment])
         .launch();
 }
