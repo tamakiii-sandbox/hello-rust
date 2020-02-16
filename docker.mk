f27fdc61b771955997263326b8285e2b079e8217
tamakiii <tamakiii@users.noreply.github.com>
2020-02-16 10:08:36 +0900

    add port

--- docker.mk
+++ docker.mk
@@ -6,6 +6,7 @@ NAME := tamakiii-sandbox/hello-rust
 WORK := /app
 ENVIRONMENT := $(shell grep '^ENVIRONMENT=' .env | sed -E 's/ENVIRONMENT=//')
 VOLUMES := $(DIR):$(WORK) ~/.gitignore:/root/.gitignore ~/.config/git/ignore:/root/.config/git/ignore
+PORTS := 8800:8000
 
 CMD := bash
 VSC_NAME := vsc-hello-rust-
@@ -25,16 +26,16 @@ build:
 	docker build -t $(NAME) --target $(ENVIRONMENT) .
 
 run:
-	docker run --rm -it $(foreach v,$(VOLUMES),-v $v) -w $(WORK) $(NAME) make run
+	docker run --rm -it $(foreach p,$(PORTS),-p $p) $(foreach v,$(VOLUMES),-v $v) -w $(WORK) $(NAME) make run
 
 ash: run/ash
 bash: run/bash
 
 run/%:
-	docker run --rm -it $(foreach v,$(VOLUMES),-v $v) -w $(WORK) $(NAME) $(@F)
+	docker run --rm -it $(foreach p,$(PORTS),-p $p) $(foreach v,$(VOLUMES),-v $v) -w $(WORK) $(NAME) $(@F)
 
 make/%:
-	docker run --rm -it $(foreach v,$(VOLUMES),-v $v) -w $(WORK) $(NAME) make $(@F)
+	docker run --rm -it $(foreach p,$(PORTS),-p $p) $(foreach v,$(VOLUMES),-v $v) -w $(WORK) $(NAME) make $(@F)
 
 vsc:
 	docker exec -it -w /workspaces/$(shell basename $(realpath $(dir $(lastword $(MAKEFILE_LIST))))) $$(docker container ls | grep $(VSC_NAME) | awk '{ print $$1 }') $(CMD)
