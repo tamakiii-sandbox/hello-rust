b77f7d4e0188441527308a3d575f061355c78228
tamakiii <tamakiii@users.noreply.github.com>
2020-02-22 22:10:09 +0900

    using syn

--- proc_macro/src/lib.rs
+++ proc_macro/src/lib.rs
@@ -1,9 +1,12 @@
 extern crate proc_macro;
 
 use crate::proc_macro::TokenStream;
+use syn::{parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
 pub fn trace(_attr: TokenStream, item: TokenStream) -> TokenStream {
-  let item = dbg!(item);
-  item
+  let ret = item.clone();
+  let ast = parse_macro_input!(item as ItemFn);
+  dbg!(ast);
+  ret
 }
