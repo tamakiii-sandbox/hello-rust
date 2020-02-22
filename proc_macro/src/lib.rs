127d414f5b2e9a6f8ffdf65a1576082fadac8b31
tamakiii <tamakiii@users.noreply.github.com>
2020-02-22 22:28:38 +0900

    using quote

--- proc_macro/src/lib.rs
+++ proc_macro/src/lib.rs
@@ -1,12 +1,23 @@
 extern crate proc_macro;
 
 use crate::proc_macro::TokenStream;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, ItemFn, Stmt};
+use quote::quote;
 
 #[proc_macro_attribute]
 pub fn trace(_attr: TokenStream, item: TokenStream) -> TokenStream {
-  let ret = item.clone();
-  let ast = parse_macro_input!(item as ItemFn);
-  dbg!(ast);
-  ret
+  let mut ast = parse_macro_input!(item as ItemFn);
+
+  let new_stmt = quote!{ println!("trace"); };
+  let new_stmt: TokenStream = new_stmt.into();
+  let new_stmt = parse_macro_input!(new_stmt as Stmt);
+
+  ast.block.stmts.clear();
+  ast.block.stmts.push(new_stmt);
+
+  let gen = quote! {
+    #ast
+  };
+
+  gen.into()
 }
