25e54db2715995e7b7e162f881f4168ed08f253e
tamakiii <tamakiii@users.noreply.github.com>
2020-02-25 15:01:32 +0900

    &ast.ident is unknown field

--- proc_macro/src/lib.rs
+++ proc_macro/src/lib.rs
@@ -1,23 +1,51 @@
 extern crate proc_macro;
 
 use crate::proc_macro::TokenStream;
-use syn::{parse_macro_input, ItemFn, Stmt};
 use quote::quote;
+use syn::{parse_macro_input, ItemFn, Stmt};
 
 #[proc_macro_attribute]
 pub fn trace(_attr: TokenStream, item: TokenStream) -> TokenStream {
-  let mut ast = parse_macro_input!(item as ItemFn);
+    let mut ast = parse_macro_input!(item as ItemFn);
+    let ident = &ast.ident;
 
-  let new_stmt = quote!{ println!("trace"); };
-  let new_stmt: TokenStream = new_stmt.into();
-  let new_stmt = parse_macro_input!(new_stmt as Stmt);
+    let enter = quote! {
+        println!("Enter: {}", stringify!(#ident) );
+    };
+    let enter: TokenStream = enter.into();
+    let enter = parse_macro_input!(enter as Stmt);
 
-  ast.block.stmts.clear();
-  ast.block.stmts.push(new_stmt);
+    let mut body = quote! {};
+    for s in &ast.block.stmts {
+        body = quote! {
+            #body
+            #s
+        };
+    }
+    let body = quote! {
+        let body = || { #body };
+    };
+    let body: TokenStream = body.into();
+    let body = parse_macro_input!(body as Stmt);
 
-  let gen = quote! {
-    #ast
-  };
+    let exit = quote! {
+        {
+            let ret = body();
+            println!("Exit: {}", stringify!(#ident) );
+            ret
+        }
+    };
+    let exit: TokenStream = exit.into();
+    let exit = parse_macro_input!(exit as Stmt);
 
-  gen.into()
+    ast.block.stmts.clear();
+    ast.block.stmts.push(enter);
+    ast.block.stmts.push(body);
+    ast.block.stmts.push(exit);
+
+    let gen = quote! {
+        #ast
+    };
+
+    gen.into()
 }
