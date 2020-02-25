aaa2f0f759b017a2896297641213ba4e34e15068
tamakiii <tamakiii@users.noreply.github.com>
2020-02-25 15:15:52 +0900

    no vec! Vec.push

--- experiment/macros/src/main.rs
+++ experiment/macros/src/main.rs
@@ -1,4 +1,12 @@
+
 fn main() {
-    let x: Vec<u32> = vec![1, 2, 3];
+    let x: Vec<u32> = {
+        let mut tmp = Vec::new();
+        tmp.push(1);
+        tmp.push(2);
+        tmp.push(3);
+        tmp
+    };
+
     println!("hello: {:?}", x);
 }
